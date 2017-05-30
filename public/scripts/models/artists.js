a206d9f57f7108bc146ebe996bb8f1309ac2be2a
dustin <dbyers84@gmail.com>
2017-05-30 16:51:32 -0700

    fixed our shit, thanks Gio

--- public/scripts/models/artists.js
+++ public/scripts/models/artists.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var app = app || {};
@@ -15,58 +16,51 @@ let topTracks = [];
     });
   };
 
-
-
-
-
-
   artists.getTopTracks = function(artistSub) {
     $.ajax({
       type : 'GET',
       url : 'http://ws.audioscrobbler.com/2.0/',
-      data : {method: 'artist.getInfo', artists: artistSub, api_key: '57ee3318536b23ee81d6b27e36997cde', format: 'jsonp'},
-      dataType : 'jsonp',
-      success : function(data) {
-        let $artistList = $('<ul>').addClass('artistList').appendTo('#artist');
+      data : {method: 'artist.getinfo', artist: artistSub, api_key: '57ee3318536b23ee81d6b27e36997cde', format: 'json'},
+      dataType: 'json',
 
-        for (var i=0; i<data.artist.similar.artist.length; i++){
-          similarArtists.push(data.artist.similar.artist[i].name);
-          $(`<li>${similarArtists[i]}</li>`).appendTo($artistList);
+    }).then(function(data){
+      console.log(data);
+      // app.artists.getSimilarTracks();
 
-          $.ajax({
-            type : 'GET',
-            url : 'http://ws.audioscrobbler.com/2.0/',
-            data : {method: 'artist.getTopTracks', artist: artistSub, api_key: '57ee3318536b23ee81d6b27e36997cde', format: 'jsonp'},
-            dataType : 'jsonp',
-            success : function(data) {
-              // console.log(data.toptracks);
-              topTracks.push(data.toptracks);
-            },
-            error : function(code, message){
-              $('#error').html('Error Code: ' + code + ', Error Message: ' + message);
-            }
-          });
-        }
+      let $artistList = $('<ul>').addClass('artistList').appendTo('#artist');
+      let similarArtistsRequests = [];
 
+      for (var i=0; i<data.artist.similar.artist.length; i++){
+        similarArtists.push(data.artist.similar.artist[i].name);
+        $(`<li>${similarArtists[i]}</li>`).appendTo($artistList);
+
+        similarArtistsRequests.push($.ajax({
+          type : 'GET',
+          url : 'http://ws.audioscrobbler.com/2.0/',
+          data : {method: 'artist.gettoptracks', artist: artistSub, api_key: '57ee3318536b23ee81d6b27e36997cde', format: 'json'},
+          dataType : 'json',
+        }))
       }
+      return Promise.all(similarArtistsRequests)
 
-    }).then(function(){
-      app.artists.getSimilarTracks();
-    });
+    })
+    .then(function(data) {
+      console.log(data);
+    })
     // console.log(topTracks);
     // console.log(similarArtists);
   };
 
-  artists.getSimilarTracks = function() {
-    for (var i=0; i< similarArtists.length; i++) {
-
-      let artist = similarArtists[i];
-      console.log(artist);
-
-      let track = topTracks[i];
-      console.log(track);
-
-    }
+  // artists.getSimilarTracks = function() {
+  //   for (var i=0; i< similarArtists.length; i++) {
+  //
+  //     let artist = similarArtists[i];
+  //     console.log(artist);
+  //
+  //     let track = topTracks[i];
+  //     console.log(track);
+  //
+  //   }
   //   $.ajax({
   //     type : 'GET',
   //     url : 'http://ws.audioscrobbler.com/2.0/',
@@ -81,7 +75,7 @@ let topTracks = [];
   //       $('#error').html('Error Code: ' + code + ', Error Message: ' + message);
   //     }
   //   });
-  };
+  // };
 
   module.artists = artists;
 })(app);
